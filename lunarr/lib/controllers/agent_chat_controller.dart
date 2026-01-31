f52e78e2dd8a7710b5ba56cf75522cf7f544ba0f
Seungho Jang <develop0235@gmail.com>
2026-02-01 01:16:23 +0900

    update view/agent_chat, controller/agent_chat

--- lunarr/lib/controllers/agent_chat_controller.dart
+++ lunarr/lib/controllers/agent_chat_controller.dart
@@ -1,13 +1,45 @@
 import 'package:lunarr/models/agent_card_model.dart';
 
 class AgentChatController {
+  bool _lock = false;
+  List<List<AgentCardModel>> _agentCardModelss = [
+    [
+      AgentCardModel.seungho(false),
+      AgentCardModel.kyungho(true),
+      AgentCardModel.minseok(true),
+      AgentCardModel.seungho(true),
+    ],
+    [
+      AgentCardModel.seungho(true),
+      AgentCardModel.kyungho(false),
+      AgentCardModel.minseok(true),
+      AgentCardModel.seungho(true),
+    ],
+  ];
+
+  bool get lock => _lock;
+  List<List<AgentCardModel>> get agentCardModelss => _agentCardModelss;
+
   // TODO: integrate API
-  Future<List<AgentCardModel>> getAgentCardModels() async {
-    return [
+  Future<bool> getAgentCardModels() async {
+    if (_lock) return false;
+    _lock = true;
+
+    List<AgentCardModel> _agentCardModels = [
       AgentCardModel.seungho(true),
       AgentCardModel.kyungho(true),
       AgentCardModel.minseok(true),
       AgentCardModel.seungho(true),
     ];
+
+    _agentCardModelss.add(_agentCardModels);
+
+    return true;
+  }
+
+  // TODO: integrate API using _agentCardModels
+  Future<bool> getAgentChatModel() async {
+    _lock = false;
+    return true;
   }
 }
--- lunarr/lib/views/agent_chat_view.dart
+++ lunarr/lib/views/agent_chat_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:lunarr/controllers/agent_chat_controller.dart';
+import 'package:lunarr/models/agent_card_model.dart';
 import 'package:lunarr/models/agent_model.dart';
 import 'package:lunarr/services/agent_service.dart';
+import 'package:lunarr/widgets/agent_card_widget.dart';
 
 class AgentChatView extends StatefulWidget {
   const AgentChatView({super.key});
@@ -10,13 +13,192 @@ class AgentChatView extends StatefulWidget {
 }
 
 class _AgentChatViewState extends State<AgentChatView> {
+  final AgentModel am = AgentService().agentModel;
+  final AgentChatController acc = AgentChatController();
+
   @override
   Widget build(BuildContext context) {
     ColorScheme cs = Theme.of(context).colorScheme;
     TextTheme tt = Theme.of(context).textTheme;
-    AgentModel agentModel = AgentService().agentModel;
 
-    return Column(children: [_buildAppBar(agentModel, tt, cs)]);
+    return Stack(
+      children: [
+        Column(
+          spacing: 24,
+          children: [
+            _buildAppBar(am, tt, cs),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  spacing: 24,
+                  children: [
+                    ...acc.agentCardModelss.map(
+                      (acms) => _buildAgentCards(
+                        acms,
+                        cs,
+                        tt,
+                        acc.lock && acms == acc.agentCardModelss.last,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+        IconButton(
+          onPressed: () async {
+            await acc.getAgentCardModels();
+            setState(() {});
+          },
+          icon: Icon(Icons.send),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildAgentCards(
+    List<AgentCardModel> acms,
+    ColorScheme cs,
+    TextTheme tt,
+    bool enabled,
+  ) {
+    if (enabled) {
+      final ValueNotifier<int> areSelectedCount = ValueNotifier<int>(0);
+
+      void updateAreSelectedCount() {
+        areSelectedCount.value = acms.where((e) => e.isSelected).length;
+      }
+
+      void deleteAreSelectedCount() {
+        areSelectedCount.dispose();
+      }
+
+      updateAreSelectedCount();
+
+      final ValueNotifier<bool> isConfirmed = ValueNotifier<bool>(false);
+
+      void updateIsConfirmed() {
+        isConfirmed.value = !isConfirmed.value;
+      }
+
+      void deleteIsConfirmed() {
+        isConfirmed.dispose();
+      }
+
+      return SizedBox(
+        width: 720,
+        child: Column(
+          spacing: 24,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ValueListenableBuilder<int>(
+                  valueListenable: areSelectedCount,
+                  builder: (context, count, child) {
+                    return Text(
+                      '$count Agents Selected',
+                      style: tt.titleLarge?.copyWith(color: cs.onSurface),
+                    );
+                  },
+                ),
+                ValueListenableBuilder<bool>(
+                  valueListenable: isConfirmed,
+                  builder: (context, value, child) {
+                    return FilledButton(
+                      onPressed: isConfirmed.value
+                          ? null
+                          : () {
+                              updateIsConfirmed();
+                              deleteAreSelectedCount();
+                              deleteIsConfirmed();
+                            },
+                      child: Text('Confirm'),
+                    );
+                  },
+                ),
+              ],
+            ),
+            Column(
+              spacing: 8,
+              children: [
+                for (int i = 0; i < acms.length; i += 2)
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    spacing: 8,
+                    children: [
+                      ValueListenableBuilder<bool>(
+                        valueListenable: isConfirmed,
+                        builder: (context, value, child) {
+                          return AgentCardWidget(
+                            agentCardModel: acms[i],
+                            onTap: isConfirmed.value
+                                ? null
+                                : () {
+                                    updateAreSelectedCount();
+                                  },
+                          );
+                        },
+                      ),
+                      if (i + 1 < acms.length) ...[
+                        ValueListenableBuilder<bool>(
+                          valueListenable: isConfirmed,
+                          builder: (context, value, child) {
+                            return AgentCardWidget(
+                              agentCardModel: acms[i + 1],
+                              onTap: isConfirmed.value
+                                  ? null
+                                  : () {
+                                      updateAreSelectedCount();
+                                    },
+                            );
+                          },
+                        ),
+                      ],
+                    ],
+                  ),
+              ],
+            ),
+          ],
+        ),
+      );
+    } else {
+      return SizedBox(
+        width: 720,
+        child: Column(
+          spacing: 24,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '${acms.where((e) => e.isSelected).length} Agents Selected',
+                  style: tt.titleLarge?.copyWith(color: cs.onSurface),
+                ),
+                FilledButton(onPressed: null, child: Text('Confirm')),
+              ],
+            ),
+            Column(
+              spacing: 8,
+              children: [
+                for (int i = 0; i < acms.length; i += 2)
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    spacing: 8,
+                    children: [
+                      AgentCardWidget(agentCardModel: acms[i]),
+                      if (i + 1 < acms.length) ...[
+                        AgentCardWidget(agentCardModel: acms[i + 1]),
+                      ],
+                    ],
+                  ),
+              ],
+            ),
+          ],
+        ),
+      );
+    }
   }
 
   Widget _buildAppBar(AgentModel agentModel, TextTheme tt, ColorScheme cs) {
